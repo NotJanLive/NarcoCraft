8c1e6d41f7b61076f19fa5b777d8d17057f34ee0
NotJanLive <notjan2006@gmail.com>
2024-12-02 23:06:45 +0100

    Initial commit

---
+++ src/main/java/de/notjan/NarcoCraft.java
@@ -0,0 +1,15 @@
+package de.notjan;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NarcoCraft implements ModInitializer {
+	public static final String MOD_ID = "narcocraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/de/notjan/NarcoCraftClient.java
@@ -0,0 +1,10 @@
+package de.notjan;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NarcoCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
